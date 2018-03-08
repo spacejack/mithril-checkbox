dd78cc0d089fa1f4833b06f459de10cf03d046cb
spacejack <spacejack@gmail.com>
2018-03-07 20:56:30 -0500

    Add radio input type

--- example/src/main.ts
+++ example/src/main.ts
@@ -2,18 +2,33 @@ import * as m from 'mithril'
 import mCheckbox from '../../src'
 
 let checked = false
+let radioed = false
 
 // Demo component
 const demoComponent = {
 	view() {
 		return m('.demo',
-			m(mCheckbox,
-				{
-					id: 'chk-test',
-					checked,
-					onclick() {checked = !checked}
-				},
-				'Click Me'
+			m('p',
+				m(mCheckbox,
+					{
+						id: 'chk-test',
+						type: 'checkbox',
+						checked: checked,
+						onclick() {checked = !checked}
+					},
+					'Check Me!'
+				)
+			),
+			m('p',
+				m(mCheckbox,
+					{
+						id: 'rad-test',
+						type: 'radio',
+						checked: radioed,
+						onclick() {radioed = !radioed}
+					},
+					'Radio Me!'
+				)
 			)
 		)
 	}
