f069424f98287c64fa1e26c230ce6f426ba0399f
spacejack <spacejack@gmail.com>
2020-04-21 14:44:21 -0400

    Revert to commonjs module format, update packages

--- change-log.md
+++ change-log.md
@@ -1,8 +1,16 @@
 # Change log
 
-## v0.3.0
+## v1.2.0
 
-* Revert to commonjs module format.
+* Revert to commonjs module, update packages
+
+## v1.1.0
+
+* Update packages
+
+## v1.0.0
+
+* Update to Mithril 2.0
 
 ## v0.2.0
 
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+"use strict";
 var __rest = (this && this.__rest) || function (s, e) {
     var t = {};
     for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
@@ -9,40 +10,29 @@ var __rest = (this && this.__rest) || function (s, e) {
         }
     return t;
 };
-(function (factory) {
-    if (typeof module === "object" && typeof module.exports === "object") {
-        var v = factory(require, exports);
-        if (v !== undefined) module.exports = v;
-    }
-    else if (typeof define === "function" && define.amd) {
-        define(["require", "exports", "mithril"], factory);
-    }
-})(function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    var m = require("mithril");
-    var MithrilCheckbox = {
-        // Separate label attributes and input attributes
-        // and remove `key` attribute since it should not
-        // be used within this view.
-        view: function (_a) {
-            var _b = _a.attrs, key = _b.key, inputClass = _b.class, className = _b.className, idFor = _b.for, style = _b.style, inputAttrs = __rest(_b, ["key", "class", "className", "for", "style"]), children = _a.children;
-            // Build label attrs object
-            var labelAttrs = {};
-            if (className !== undefined) {
-                labelAttrs.className = className;
-            }
-            else if (inputClass !== undefined) {
-                labelAttrs.class = inputClass;
-            }
-            if (style !== undefined) {
-                labelAttrs.style = style;
-            }
-            if (idFor !== undefined) {
-                labelAttrs.for = idFor;
-            }
-            return m('label.mithril-checkbox', labelAttrs, m('input', inputAttrs), m('div.mithril-checkbox-shape'), m('span.mithril-checkbox-children', children));
+Object.defineProperty(exports, "__esModule", { value: true });
+var m = require("mithril");
+var MithrilCheckbox = {
+    // Separate label attributes and input attributes
+    // and remove `key` attribute since it should not
+    // be used within this view.
+    view: function (_a) {
+        var _b = _a.attrs, key = _b.key, inputClass = _b.class, className = _b.className, idFor = _b.for, style = _b.style, inputAttrs = __rest(_b, ["key", "class", "className", "for", "style"]), children = _a.children;
+        // Build label attrs object
+        var labelAttrs = {};
+        if (className !== undefined) {
+            labelAttrs.className = className;
         }
-    };
-    exports.default = MithrilCheckbox;
-});
+        else if (inputClass !== undefined) {
+            labelAttrs.class = inputClass;
+        }
+        if (style !== undefined) {
+            labelAttrs.style = style;
+        }
+        if (idFor !== undefined) {
+            labelAttrs.for = idFor;
+        }
+        return m('label.mithril-checkbox', labelAttrs, m('input', inputAttrs), m('div.mithril-checkbox-shape'), m('span.mithril-checkbox-children', children));
+    }
+};
+exports.default = MithrilCheckbox;
